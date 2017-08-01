e6c865a6c4ed60571bb5110da22069a3ac68577e
JolitaAG <jolita278@gmail.com>
2017-08-01 10:04:56 +0300

    added  state constants in FastTyping function object, added style for register state page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-/**
- * Created by Jolita A-G on 8/1/2017.
- */
+var FastTyping = function () //function object
+{
+    const STATE_REGISTER = "register",   //game states
+        STATE_LEVEL_SELECTION = "level_selection",
+        STATE_GAME = "game",
+        STATE_GAME_OVER ="game_over";
+    
+    alert('working!');
+    //initialize()
+}
+
+function initialize() {
+
+}
